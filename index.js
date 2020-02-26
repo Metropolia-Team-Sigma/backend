8d0850e136772c07b3578e79adb09e9219cd8ed0
Linus Willner <hello@linuswillner.me>
2020-02-26 11:33:06 +0200

    Update startup process

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Load environment variables and global libraries
 require('dotenv-safe').config()
 require('colors')
+require('./src/log')
 
 const checkDbConnection = require('./src/startup/checkDbConnection')
+const startWsServer = require('./src/ws')
 
 // Startup sequence
 ;(async () => {
   try {
     await checkDbConnection()
+    await startWsServer()
+    global.log.info('Startup complete, Sigma is ready for business!')
   } catch (err) {
-    console.error(`Startup failed: ${err}`.red)
+    global.log.error(`Startup failed: ${err}`.red)
     process.exit(1)
   }
 })()
