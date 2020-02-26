396dbcfb7ef7f249bb46de586540549769abcad9
Linus Willner <hello@linuswillner.me>
2020-02-26 11:31:40 +0200

    Add socket caching to WS

--- src/ws/index.js
+++ src/ws/index.js
@@ -2,35 +2,65 @@ const db = require('../db')
 const server = require('http').createServer()
 const io = require('socket.io')(server)
 
-server.listen(3000)
+// Initiate socket cache
+const sockets = {}
 
-io.on('connection', socket => {
-  console.debug(`Socket ${socket.id} initialised connection. Requesting identification...`.blue)
+module.exports = () => {
+  return new Promise(resolve => {
+    io.on('connection', socket => {
+      global.log.debug(`Socket ${socket.id} initialised connection. Requesting identification...`)
 
-  // Request identification for what room to join
-  socket.emit('request_identify', {})
+      // Add socket to cache
+      sockets[socket.id] = { hasIdentified: false }
 
-  socket.on('identify', async data => {
-    console.debug(`Socket ${socket.id} identified for joining room ${data.room}.`.blue)
+      setTimeout(() => {
+        // Check if socket is still hanging without having identified after grace period
+        const potentiallyDormantSocket = sockets[socket.id]
+        if (potentiallyDormantSocket && !potentiallyDormantSocket.hasIdentified) {
+          global.log.warn(`Socket ${socket.id} did not identify within grace period, disconnecting.`)
+          socket.emit('error', { message: 'Did not identify within timeout period' })
+          socket.disconnect()
+        }
+      }, 3000)
 
-    // Check if room being joined exists
-    const roomExists = await db.roomExists(data.room)
-    if (!roomExists) {
-      console.warn(`Socket ${socket.id} attempted to join non-existent room ${data.room}, disconnecting.`.yellow)
-      socket.emit('error', { code: 404, message: 'Room not found' })
-      socket.disconnect()
-    } else {
-      socket.join(data.room)
-      socket.emit('room_connected', {})
-      console.log(`Socket ${socket.id} joined room ${data.room}.`)
-    }
+      // Request identification for what room to join
+      socket.emit('request_identify', {})
+
+      socket.on('identify', async data => {
+        global.log.debug(`Socket ${socket.id} identified for joining room ${data.room}.`)
+
+        // Check if room being joined exists
+        const roomExists = await db.roomExists(data.room)
+        if (!roomExists) {
+          global.log.warn(`Socket ${socket.id} attempted to join non-existent room ${data.room}, disconnecting.`)
+          socket.emit('error', { message: 'Room not found' })
+          socket.disconnect()
+        } else {
+          socket.join(data.room)
+          socket.emit('room_connected', {})
+          global.log.info(`Socket ${socket.id} joined room ${data.room}.`)
+        }
+      })
+
+      socket.on('error', err => {
+        global.log.error(`Socket ${socket.id} encountered error: `, err)
+      })
+
+      socket.on('disconnect', reason => {
+        global.log.debug(`Socket ${socket.id} disconnected, reason: ${reason}`)
+        // If socket is still in cache, delete
+        if (sockets[socket.id]) delete sockets[socket.id]
+
+        sockets[socket.id]
+          ? global.log.silly(`Socket ${socket.id} flushed from cache.`)
+          : global.log.silly(`Socket ${socket.id} was not in cache, skipping purge.`)
+      })
+    })
+
+    const port = 3000
+    server.listen(port, () => {
+      global.log.info(`WebSocket server started on http://localhost:${port}.`)
+      resolve()
+    })
   })
-
-  socket.on('error', err => {
-    console.error(`Socket ${socket.id} encountered error: `.red, err)
-  })
-
-  socket.on('disconnect', reason => {
-    console.debug(`Socket ${socket.id} disconnected, reason: ${reason}`.blue)
-  })
-})
+}
