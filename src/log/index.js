2cafaa0288342cb8ec1214ef6bff1323af9d3d09
Linus Willner <hello@linuswillner.me>
2020-02-26 17:47:08 +0200

    Add prefix generation to logs

--- src/log/index.js
+++ src/log/index.js
@@ -1,10 +1,20 @@
 const winston = require('winston')
 
+const generatePrefix = info => {
+  let prefix
+
+  if (info.socket) prefix = `[socket ${info.socket}]`
+  else prefix = '[sigma-backend]'
+
+  return `${prefix} ${info.level}: ${info.message}`
+}
+
 const logger = winston.createLogger({
   level: process.env.NODE_ENV === 'debug' ? 'silly' : 'info',
   format: winston.format.combine(
     winston.format.colorize(),
-    winston.format.simple()
+    winston.format.simple(),
+    winston.format.printf(info => generatePrefix(info))
   ),
   transports: [
     new winston.transports.Console()
