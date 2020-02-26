50e86b6fb239339be60038954bb2d704d4b9d8da
Linus Willner <hello@linuswillner.me>
2020-02-26 18:23:12 +0200

    Properly sanitise room join data

--- src/ws/handlers/request_room_join.js
+++ src/ws/handlers/request_room_join.js
@@ -1,15 +1,21 @@
+const stringify = require('node-stringify')
+
 module.exports = async (data, socket, db) => {
-  global.log.debug(`Requested to join room ${data.room}.`, { socket: socket.id })
+  // Sanitise room to string in case other types of data were passed
+  // Using external module as JSON.stringify(undefined) > undefined
+  const room = stringify(data.room)
+
+  global.log.debug(`Requested to join room ${room}.`, { socket: socket.id })
 
   // Check if room being joined exists
-  const roomExists = await db.roomExists(data.room)
+  const roomExists = await db.roomExists(room)
   if (!roomExists) {
-    global.log.warn(`Attempted to join non-existent room ${data.room}, disconnecting.`, { socket: socket.id })
+    global.log.warn(`Attempted to join non-existent room ${room}, disconnecting.`, { socket: socket.id })
     socket.emit('error', { message: 'Room not found' })
     socket.disconnect()
   } else {
-    socket.join(data.room)
+    socket.join(room)
     socket.emit('room_joined', {})
-    global.log.info(`Joined room ${data.room}.`, { socket: socket.id })
+    global.log.info(`Joined room ${room}.`, { socket: socket.id })
   }
 }
