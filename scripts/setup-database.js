bd3706a27d409a49e7a62d336a4d30bb77e44149
Linus Willner <hello@linuswillner.me>
2020-02-26 11:32:55 +0200

    Improve DB setup script

--- scripts/setup-database.js
+++ scripts/setup-database.js
@@ -1,21 +1,24 @@
 require('dotenv-safe').config()
+require('colors')
 
-console.log('Running database setup...')
+console.log('Running database setup...'.green)
 
 const driver = require('arangojs')(process.env.ARANGO_URI)
 driver.useBasicAuth(process.env.ARANGO_USER, process.env.ARANGO_PASS)
 
+const db = process.env.ARANGO_DATABASE
+const coll = 'rooms'
+
 driver.listDatabases().then(async databases => {
-  if (databases.includes(process.env.ARANGO_DATABASE)) {
-    console.log('Database exists, checking for collection "rooms"...')
-    driver.useDatabase(process.env.ARANGO_DATABASE)
+  if (databases.includes(db)) {
+    console.log(`Database "${db}" exists, checking for collection "${coll}"...`.green)
+    driver.useDatabase(db)
 
     return driver.listCollections()
   } else {
-    console.log('Database does not exist, creating...')
-    await driver.createDatabase(process.env.ARANGO_DATABASE)
-
-    console.log('Database created, creating collection "rooms"...')
+    console.log(`Database "${db}" does not exist, creating...`.yellow)
+    await driver.createDatabase(db)
+    console.log(`Database "${db}" created, creating collection "${coll}"...`.green)
     driver.useDatabase(process.env.ARANGO_DATABASE)
 
     return driver.listCollections()
@@ -23,9 +26,12 @@ driver.listDatabases().then(async databases => {
 }).then(async collections => {
   const collectionNames = collections.map(collection => collection.name)
 
-  if (collectionNames.includes('rooms')) console.log('Collection "rooms" already exists.')
+  if (collectionNames.includes(coll)) console.log(`Collection "${coll}" exists.`.green)
   else {
-    await driver.collection('rooms').create()
-    console.log('Collection "rooms" has been created.')
+    console.log(`Collection "${coll}" does not exist, creating...`.yellow)
+    await driver.collection(coll).create()
+    console.log(`Collection "${coll}" created.`.green)
   }
+
+  console.log('Finished!'.green)
 })
