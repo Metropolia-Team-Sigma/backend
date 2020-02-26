70c65fdd36eeba99c5f1d4e95d5f6156be06e41f
Linus Willner <hello@linuswillner.me>
2020-02-26 11:35:36 +0200

    Make debug mode explicit

--- src/log/index.js
+++ src/log/index.js
@@ -1,7 +1,7 @@
 const winston = require('winston')
 
 const logger = winston.createLogger({
-  level: process.env.NODE_ENV !== 'production' ? 'silly' : 'info',
+  level: process.env.NODE_ENV === 'debug' ? 'silly' : 'info',
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.simple()
