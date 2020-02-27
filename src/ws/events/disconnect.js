7c83a59d95a4cfe427b472b46bc3e5245f86e7ad
Linus Willner <hello@linuswillner.me>
2020-02-27 22:20:26 +0200

    Add disconnect reason parsing for better readability

--- src/ws/events/disconnect.js
+++ src/ws/events/disconnect.js
@@ -1,7 +1,18 @@
+const parseDisconnectReason = reason => {
+  switch (reason) {
+    case 'transport error': return 'Transport error'
+    case 'server namespace disconnect': return 'Server-initiated manual disconnect'
+    case 'client namespace disconnect': return 'Client-initiated manual disconnect'
+    case 'transport close': return 'Client stopped sending data'
+    case 'ping timeout': return 'Client did not respond within ping timeout period'
+  }
+}
+
 module.exports = state => {
   const { eventParams, socket, socketCache } = state
   const reason = eventParams
-  global.log.debug(`Disconnected, reason: ${reason}`, { socket: socket.id })
+
+  global.log.debug(`Disconnected, reason: ${parseDisconnectReason(reason)}`, { socket: socket.id })
 
   // If socket is still in cache, delete
   if (socketCache[socket.id]) delete socketCache[socket.id]
